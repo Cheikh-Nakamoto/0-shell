289cc6091af51a133618d18f4db39dcf581bde81
Franchis Janel MOKOMBA <proverbes2019@gmail.com>
2025-01-23 20:46:49 +0000

    0-shell is finished

--- src/commands/mkdir.rs
+++ src/commands/mkdir.rs
@@ -3,6 +3,24 @@ use std::path::Path;
 use crate::utils::error::ShellError;
 use crate::utils::messages::NO_DIRECTORY_SPECIFIED;
 
+/**
+    * Create directories.
+    *
+    * # Arguments
+    * * `current_dir` - The current directory.
+    * * `args` - The arguments passed to the command.
+    *
+    * # Examples
+    * ```
+    * use std::path::Path;
+    * use shell::commands::mkdir;
+    *
+    * let current_dir = Path::new("/tmp");
+    * let args = ["dir1", "dir2"];
+    * let result = mkdir(current_dir, &args);
+    * assert!(result.is_ok());
+    * ```
+*/
 pub fn mkdir(current_dir: &Path, args: &[&str]) -> Result<(), ShellError> {
     if args.is_empty() {
         return Err(ShellError::InvalidArguments(NO_DIRECTORY_SPECIFIED.to_owned()))
